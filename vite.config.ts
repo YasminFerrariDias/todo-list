cc2bc8b8b7e1cc44db8f5f144a1e67d75952f983
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-03-25 14:46:18 -0300

    Criando o componente Icon

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import svgr from 'vite-plugin-svgr'
 import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
@@ -8,6 +9,7 @@ export default defineConfig({
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
-    tailwindcss()
+    tailwindcss(),
+    svgr()
   ],
 })
