804b75a644c32f44fdc38be39b8078d25c7de51e
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-03-26 15:21:19 -0300

    Deletar a tarefa

--- src/hooks/use-task.ts
+++ src/hooks/use-task.ts
@@ -22,11 +22,15 @@ export default function useTask() {
 
   function updateTaskStatus(id: string, concluded: boolean) {
     setTasks(
-      tasks.map((task) => task.id === id ? {...task, concluded}: task)
+      tasks.map((task) => task.id === id ? { ...task, concluded } : task)
     )
   }
 
+  function deleteTask(id: string) {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
   return {
-    updateTask, prepareTask, updateTaskStatus
+    updateTask, prepareTask, updateTaskStatus, deleteTask
   }
 }
