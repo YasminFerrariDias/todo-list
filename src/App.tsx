f2d7381c0be8e1cbfbd96ed4251c4e55f6f77939
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-03-26 10:36:03 -0300

    Criando o Componente Skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -13,55 +13,70 @@ import ButtonIcon from "./components/button-icon"
 import InputText from "./components/input-text";
 import InputCheckbox from "./components/input-checkbox"
 import Card from "./components/card"
+import Container from "./components/container"
+import Skeleton from "./components/skeleton"
 
 export default function App() {
   return (
-    <div>
-      <div className="flex flex-col gap-2">
-        <Text variant="body-sm-bold" className="text-pink-base">
-          Olá mundo!
-        </Text>
-        <Text className="text-green-base">
-          Olá mundo!
-        </Text>
-        <Text variant="body-md-bold">
-          Olá mundo!
-        </Text>
-      </div>
-      <div className="flex gap-1">
-        <Icon svg={TrashIcon} />
-        <Icon svg={CheckIcon} />
-        <Icon svg={PencilIcon} />
-        <Icon svg={PlusIcon} />
-        <Icon svg={SpinnerIcon} animate />
-        <Icon svg={XIcon} />
-      </div>
-      <div>
-        <Badge variant="secondary">5</Badge>
-        <Badge variant="primary">2 de 5</Badge>
-      </div>
+    <Container>
+      <div className="grid gap-10">
+        <div className="flex flex-col gap-2">
+          <Text variant="body-sm-bold" className="text-pink-base">
+            Olá mundo!
+          </Text>
+          <Text className="text-green-base">
+            Olá mundo!
+          </Text>
+          <Text variant="body-md-bold">
+            Olá mundo!
+          </Text>
+        </div>
 
-      <div>
-        <Button icon={PlusIcon}>Nova Tarefa</Button>
-      </div>
+        <div className="flex gap-1">
+          <Icon svg={TrashIcon} />
+          <Icon svg={CheckIcon} />
+          <Icon svg={PencilIcon} />
+          <Icon svg={PlusIcon} />
+          <Icon svg={SpinnerIcon} animate />
+          <Icon svg={XIcon} />
+        </div>
 
-      <div className="flex gap-1">
-        <ButtonIcon icon={TrashIcon} />
-        <ButtonIcon icon={TrashIcon} variant="secondary" />
-        <ButtonIcon icon={TrashIcon} variant="tertiary" />
-      </div>
+        <div className="flex gap-1">
+          <Badge variant="secondary">5</Badge>
+          <Badge variant="primary">2 de 5</Badge>
+          <Badge loading></Badge>
+        </div>
 
-      <div>
-        <InputText />
-      </div>
+        <div>
+          <Button icon={PlusIcon}>Nova Tarefa</Button>
+        </div>
 
-      <div>
-        <InputCheckbox />
-      </div>
+        <div className="flex gap-1">
+          <ButtonIcon icon={TrashIcon} />
+          <ButtonIcon icon={TrashIcon} variant="secondary" />
+          <ButtonIcon icon={TrashIcon} variant="tertiary" />
+          <ButtonIcon icon={TrashIcon} loading />
+        </div>
 
-      <div>
-        <Card size="md">Olá mundo</Card>
+        <div>
+          <InputText />
+        </div>
+
+        <div>
+          <InputCheckbox />
+          <InputCheckbox loading />
+        </div>
+
+        <div>
+          <Card size="md">Olá mundo</Card>
+        </div>
+
+        <div className="space-y-2">
+          <Skeleton className="h-6" />
+          <Skeleton className="h-6" />
+          <Skeleton className="w-96 h-6" />
+        </div>
       </div>
-    </div>
+    </Container>
   )
 }
--- src/components/input-checkbox.tsx
+++ src/components/input-checkbox.tsx
@@ -1,24 +1,31 @@
 
-import { cva,  type VariantProps} from "class-variance-authority";
+import { cva, type VariantProps } from "class-variance-authority";
 
 import CheckIcon from "../assets/icons/check.svg?react";
 import Icon from "./icon";
+import Skeleton from "./skeleton";
 
 export const inputCheckboxWrapperVariants = cva(`
-  inline-flex items-center justify-center relative
-  group
+  inline-flex items-center justify-center relative group
 `);
 
 export const inputCheckboxVariants = cva(
   `
     appearance-none peer flex items-center justify-center cursor-pointer
-  border-2 border-solid transition overflow-hidden border-green-base
-  hover:border-green-dark hover:bg-green-dark/20
-  checked:border-green-base checked:bg-green-base
-  group-hover:checked:border-green-dark group-hover:checked:bg-green-dark
+    transition overflow-hidden
   `,
   {
     variants: {
+      variant: {
+        none: "",
+        default: `
+          border-2 border-solid border-green-base
+          hover:border-green-dark hover:bg-green-dark/20
+          checked:border-green-base checked:bg-green-base
+          group-hover:checked:border-green-dark 
+          group-hover:checked:bg-green-dark
+        `
+      },
       size: {
         md: "w-5 h-5 rounded-sm"
       },
@@ -27,6 +34,7 @@ export const inputCheckboxVariants = cva(
       }
     },
     defaultVariants: {
+      variant: "default",
       size: "md",
       disabled: false
     }
@@ -50,17 +58,37 @@ export const inputCheckboxIconVariants = cva(
   }
 )
 
-interface InputCheckboxProps extends VariantProps<typeof inputCheckboxVariants>,
-  Omit<React.ComponentProps<"input">, "size" | "disabled"> { }
+interface InputCheckboxProps
+  extends VariantProps<typeof inputCheckboxVariants>,
+  Omit<React.ComponentProps<"input">, "size" | "disabled"> {
+  loading?: boolean;
+}
 
 export default function InputCheckbox({
   size,
   disabled,
   className,
+  variant,
+  loading,
   ...props
 }: InputCheckboxProps) {
-  return <label className={inputCheckboxWrapperVariants({ className })}>
-    <input type="checkbox" className={inputCheckboxVariants({ size, disabled })} {...props} />
-    <Icon svg={CheckIcon} className={inputCheckboxIconVariants({ size })} />
-  </label>
+  if (loading) {
+    return (
+      <Skeleton
+        rounded="sm"
+        className={inputCheckboxVariants({ variant: "none", size })}
+      />
+    )
+  }
+
+  return (
+    <label className={inputCheckboxWrapperVariants({ className })}>
+      <input
+        type="checkbox"
+        className={inputCheckboxVariants({ variant, size, disabled })}
+        {...props}
+      />
+      <Icon className={inputCheckboxIconVariants({ size })} svg={CheckIcon} />
+    </label>
+  )
 }
